61cff4ac7f41ef44c458079ef1d56bb48c4f0d61
Rentib <sbitner420@tutanota.com>
2023-02-25 11:13:46 +0100

    fixed circle

--- src/circle.c
+++ src/circle.c
@@ -5,11 +5,11 @@ rysik_circle(Rysik *rys, int x, int y, int r, uint32_t color)
 {
   int rysq = r * r - 1, xsq = r * r;
 
-  horizontal_line(rys, x - r, y, 2 * r, color);
+  horizontal_line(rys, x - r, y, 2 * r + 1, color);
   for (int cx = r, cy = 1; cy <= r; rysq -= ++cy * 2 + 1) {
     while (xsq > rysq) xsq -= 1 + 2 * cx--;
-    horizontal_line(rys, x - cx, y - cy, 2 * cx, color);
-    horizontal_line(rys, x - cx, y + cy, 2 * cx, color);
+    horizontal_line(rys, x - cx, y - cy, 2 * cx + 1, color);
+    horizontal_line(rys, x - cx, y + cy, 2 * cx + 1, color);
   }
 }
 
@@ -18,11 +18,11 @@ rysik_ellipse(Rysik *rys, int x, int y, int a, int b, uint32_t color)
 {
   int absq = a * a * b * b, xbsq = absq;
   int asq = a * a, bsq = b * b;
-  
-  horizontal_line(rys, x - a, y, 2 * a, color);
+
+  horizontal_line(rys, x - a, y, 2 * a + 1, color);
   for (int cx = a, cy = 1; cy <= b; absq -= (++cy * 2 + 1) * asq) {
     while (xbsq > absq) xbsq -= (1 + 2 * cx--) * bsq;
-    horizontal_line(rys, x - cx, y - cy, 2 * cx, color);
-    horizontal_line(rys, x - cx, y + cy, 2 * cx, color);
+    horizontal_line(rys, x - cx, y - cy, 2 * cx + 1, color);
+    horizontal_line(rys, x - cx, y + cy, 2 * cx + 1, color);
   }
 }
